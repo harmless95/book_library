e35d2f3f0aa98ab9d92f5d705b55ed8a373f0e17
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-07-31 21:01:56 +0700

    add config.py

---
+++ Library_project/core/config.py
@@ -0,0 +1,42 @@
+from pydantic_settings import BaseSettings
+from pydantic import ConfigDict, BaseModel, PostgresDsn
+
+
+class RunConfig(BaseModel):
+    host: str = "0.0.0.0"
+    port: int = 8000
+
+
+class ApiV1Prefix(BaseModel):
+    prefix: str = "/v1"
+
+
+class ApiPrefix(BaseModel):
+    prefix: str = "/api_v1"
+    v1: ApiV1Prefix = ApiV1Prefix()
+
+
+class DataBaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    max_overflow: int = 50
+    pool_size: int = 10
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
+
+class Settings(BaseSettings):
+    model_config = ConfigDict(from_attributes=True)
+    run: RunConfig = RunConfig()
+    api: ApiPrefix = ApiPrefix()
+    db: ApiPrefix = DataBaseConfig
+
+
+settings = Settings()
