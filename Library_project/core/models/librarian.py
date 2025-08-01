31ec9d98f57f3c29eec3ce885aa1c4ddae1af56d
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-01 13:09:57 +0700

    create relationship librarian

--- Library_project/core/models/librarian.py
+++ Library_project/core/models/librarian.py
@@ -1,11 +1,16 @@
+from datetime import date
+
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import Date, String
 
 from .base import Base
+from .mixins.mixin_library import MixinLibrary
 
 
-class Librarian(Base):
+class Librarian(MixinLibrary, Base):
+    _library_back_populates = "librarians"
+
     name: Mapped[str] = mapped_column(String(34), nullable=False)
     surname: Mapped[str] = mapped_column(String(50), nullable=False)
-    year_of_birth: Mapped[Date] = mapped_column(Date)
+    year_of_birth: Mapped[date] = mapped_column(Date, nullable=False)
     salary: Mapped[int] = mapped_column(nullable=False)
