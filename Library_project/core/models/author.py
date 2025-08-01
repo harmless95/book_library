9bbfe8fd911b7f80a106e60ae5609fc9a02a56b6
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-01 13:08:25 +0700

    create relationship author

--- Library_project/core/models/author.py
+++ Library_project/core/models/author.py
@@ -1,5 +1,6 @@
 from typing import TYPE_CHECKING
-from sqlalchemy.orm import Mapped, mapped_column
+from datetime import date
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, Date
 
 from .base import Base
@@ -11,7 +12,17 @@ if TYPE_CHECKING:
 class Author(Base):
     name: Mapped[str] = mapped_column(String(34), nullable=False)
     surname: Mapped[str] = mapped_column(String(50), nullable=False)
-    year_of_birth: Mapped[Date] = mapped_column(Date)
-    age_recommendation: Mapped[int] = mapped_column(nullable=False)
+    year_of_birth: Mapped[date] = mapped_column(Date, nullable=False)
 
-    books: Mapped[list["Book"]] = mapped_column()
+    books: Mapped[list["Book"]] = relationship(
+        "Book",
+        back_populates="author",
+        cascade="all, delete-orphan",
+        passive_deletes=True,
+    )
+
+    def __repr__(self):
+        return f"Имя: {self.name}, Фамилия: {self.surname}, Год рождения: {self.year_of_birth}"
+
+    def to_json(self):
+        return {i.name: getattr(self, i.name) for i in self.__table__.columns}
